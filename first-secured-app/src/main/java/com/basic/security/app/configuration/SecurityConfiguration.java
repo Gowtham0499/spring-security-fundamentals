903b24233d5917a90730686cbe4120c469011af8
Gowtham0499 <gowth0499@gmail.com>
2021-09-24 11:02:12 +0530

    Added basic security

---
+++ first-secured-app/src/main/java/com/basic/security/app/configuration/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.basic.security.app.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+		UserDetails user = User.withUsername("gowtham").password(passwordEncoder.encode("password")).authorities("read").build();
+		userDetailsManager.createUser(user);
+		auth.userDetailsService(userDetailsManager);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.authorizeRequests().anyRequest().authenticated();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
---
+++ first-secured-app/src/test/java/com/basic/security/app/FirstSecuredAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.security.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecuredAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
