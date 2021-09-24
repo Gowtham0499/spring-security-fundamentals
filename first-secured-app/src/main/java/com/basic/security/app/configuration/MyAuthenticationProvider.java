62b91938f31d9786229c5340e4cf429c5f5d1baf
Gowtham0499 <gowth0499@gmail.com>
2021-09-24 11:50:00 +0530

    Added custom authentication provider

---
+++ first-secured-app/src/main/java/com/basic/security/app/configuration/MyAuthenticationProvider.java
@@ -0,0 +1,34 @@
+package com.basic.security.app.configuration;
+
+import java.util.Arrays;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = authentication.getName();
+		String password = authentication.getCredentials().toString();
+		if(username.equals("gowtham") && password.equals("password")) {
+			/*
+			 * third argument authorities is required (type list), else will throw null pointer exception
+			 */
+			return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
+		} else {
+			throw new BadCredentialsException("Invalid username or password!");
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
